af24453804f1e678260d9b1ef8834cad99160732
leo.lucidor <leo.lucidor@etu.univ-orleans.fr>
2023-11-13 12:26:49 +0100

    ajout des utilisateurs et salons pour rendu intermediaire

--- Messagerie.java
+++ Messagerie.java
@@ -1,14 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Messagerie {
+
+    private List<Salon> salons;
+    private List<Utilisateur> utilisateurs;
+
+    public Messagerie() {
+        this.salons = new ArrayList<Salon>();
+        this.utilisateurs = new ArrayList<Utilisateur>();
+        // ajout des utlisateurs et salons
+        Utilisateur leo = new Utilisateur("192.168.0.1", "leo");
+        Utilisateur erwan = new Utilisateur("192.168.0.2", "erwan");
+        this.utilisateurs.add(leo);
+        this.utilisateurs.add(erwan);
+        Salon salon = new Salon("salon", "192.168.1.1", this.utilisateurs);
+        this.salons.add(salon);
+        System.out.println(this.salons);
+    }
 
     public static void clearScreen() {  
         System.out.print("\033[H\033[2J");  
         System.out.flush();  
     }  
     public static void main(String[] args) {
-        Utilisateur leo = new Utilisateur("192.168.0.1", "leo");
-        Utilisateur erwan = new Utilisateur("192.168.0.2", "erwan");
+        Messagerie messagerie = new Messagerie();
         // IP ou nom
         Scanner inputIP = new Scanner(System.in);
         System.out.print("Entrer votre IP ou nom : ");
